b8b6b9e5e1e34bb7c1e73f7c28953ec2d85d6a1f
Maksim Ivanov <maksimivanov91@yandex.ru>
2021-05-31 20:51:58 +0300

    Create project structure, install libraries, create index.js, implementation congif default file,  db connect and server start

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+const PORT = config.get('port') || 4000
+
+app.use(cors())
+app.use(express.json())
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUrl'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log('Error in start server', e);
+    }
+}
+
+start()
